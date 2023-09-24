348173a1c5c52957452c8cbec32ff8f147e0fd22
Gabs <gsbiel62@gmail.com>
2023-09-23 21:43:52 -0400

    Revert "1 Adicionados "throws ParseException" necessários para compilar o  código; 2 Adicionado o método "cadastrar()" chamado pelo método  "apresentarMenu()". OBS: o método ainda apresenta problema ao inserir cadeias de caracter com espaços. Irei buscar uma solução para isso posteriormente. Além  disso há métodos com comentários em partes ainda não funcionais, sendo eles: método construtor Sessao (único atualmente), getters e setters do atri- buto "tipoSessao"."
    This reverts commit a02a471a2bdc8b9a6c9c7888144115912b847be0.

--- Cinema/src/main/java/ufmt_alg3/cinema/Lista.java
+++ Cinema/src/main/java/ufmt_alg3/cinema/Lista.java
@@ -1,8 +1,5 @@
 package ufmt_alg3.cinema;
-
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * @author Virgínia Aguiar
@@ -11,22 +8,11 @@ import java.util.Scanner;
  */
 
 public class Lista {
-    Scanner scanner = new Scanner(System.in);
-    ArrayList<Sessao> lista = new ArrayList();
+    
+    ArrayList<String> lista = new ArrayList();
 
-    public void cadastrar() throws ParseException {
-        System.out.println("Cadastramento de Sessão.");
-        System.out.println("Defina ID da sessão. ");
-        Integer id = scanner.nextInt();
-        System.out.println("Defina o tipo da sala.");
-        // Sala tipoSessao = new Sala(/*Parâmetros da sala.*/);
-        System.out.println("Defina o nome da sessão");
-        String nomeSessao = scanner.next();
-        System.out.println("Defina data e hora da sessão. Formatada como:");
-        System.out.print("dd/MM/yyyy HH:mm:ss");
-        String dataSessao = scanner.next();
-        Sessao sessao = new Sessao(id, nomeSessao, dataSessao);
-        lista.add(sessao);
+    public void cadastrar() {
+        
     }
 
     public void listar() {
