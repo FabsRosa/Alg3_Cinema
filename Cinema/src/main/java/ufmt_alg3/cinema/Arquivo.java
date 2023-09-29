1f811de4889ea08747c9ba40bfe9bbc8f485583b
Gabs <gsbiel62@gmail.com>
2023-09-28 22:20:12 -0400

    — Criado o atributo final "nomeDoArquivo" para referenciação no corpo do(s) método(s); — Criado o método getter de "getNomeDoArquivo()"; — Adicionado o método "salvar(ArrayList<Sessao> lista)"

--- Cinema/src/main/java/ufmt_alg3/cinema/Arquivo.java
+++ Cinema/src/main/java/ufmt_alg3/cinema/Arquivo.java
@@ -1,5 +1,8 @@
 package ufmt_alg3.cinema;
 
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * @author Virgínia Aguiar
  *         Gabriel Soares
@@ -8,8 +11,34 @@ package ufmt_alg3.cinema;
 
 public class Arquivo {
     
-    public void salvar() {
+    private final String nomeDoArquivo;
+    
+    public Arquivo () {
+        this.nomeDoArquivo = "arquivo\registrosPersistidos.ser";
+    }
+    
+    public String getNomeDoArquivo() {
+        return nomeDoArquivo;
+    }
+    
+    public void salvar(ArrayList<Sessao> lista) throws IOException {
+        File arquivo = new File(nomeDoArquivo);
         
+        // Se o arquivo não existir, então ele é criado.
+        if (!arquivo.exists()) {
+                arquivo.createNewFile();
+        }
+        
+        FileOutputStream fileOut = new FileOutputStream(getNomeDoArquivo());
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+        // Grava o ArrayList no arquivo
+        out.writeObject(lista);
+
+        out.close();
+        fileOut.close();
+
+        System.out.println("Dados persistidos com sucesso no arquivo.");
     }
 
     public void listar() {
