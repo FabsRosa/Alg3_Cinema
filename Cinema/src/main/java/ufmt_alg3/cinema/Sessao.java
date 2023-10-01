73673f33bbda0137ff758046559a59b363ed0c63
Gabs <gsbiel62@gmail.com>
2023-10-01 15:40:47 -0400

    — Adicionado comentários no padrão Javadoc; — Implementados getters e setters para o atributo proximoId; — Adicionado um novo método chamado listarSessao.

--- Cinema/src/main/java/ufmt_alg3/cinema/Sessao.java
+++ Cinema/src/main/java/ufmt_alg3/cinema/Sessao.java
@@ -6,23 +6,33 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 /**
- *
- * @author Gabriel
+ * Essa classe implementa a abstração de sessões em um cinema.
+ * 
  */
 public class Sessao implements Serializable{
     private Integer id;
-    private static Integer proximoId = 0;
+    private static Integer proximoId = 1;
     // private Sala tipoDeSessao;
     private String nomeSessao;
     private Date dataHora;
     
+    /*
+     * Esse método constutor inicializa todos os atributos da instância da 
+     * classe sessão criada. Ele, ao ser finalizado, terá uma assinatura como 
+     * segue: Sessao, String, String. 
+     * 
+     * O atributo estático proximoId é inicializado dentro da classe Lista,
+     * através do método cadastrar().
+     * @param nomeSessao, dataHora.
+     * @throws ParseException
+     */
     public Sessao (/*Sala tipoDeSessao,*/ String nomeSessao,
             String dataHora) throws ParseException {
         SimpleDateFormat dateFormat =
                 new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         this.id = proximoId;
         Sessao.proximoId++;
-        //this.tipoDeSessao = tipoDeSessao;
+        //this.tipoDeSessao = new Sala();
         this.nomeSessao = nomeSessao;
         this.dataHora = dateFormat.parse(dataHora);
     }
@@ -33,6 +43,14 @@ public class Sessao implements Serializable{
 
     public void setId(Integer id) {
         this.id = id;
+    }
+    
+    public static void setProximoId(Integer proximoId){
+        Sessao.proximoId = proximoId;
+    }
+    
+    public static Integer getProximoId() {
+        return Sessao.proximoId;
     }
 /*
     Getters e setters do tipo de sala.
@@ -61,4 +79,15 @@ public class Sessao implements Serializable{
         this.dataHora = horario;
     }
     
+    
+    /**
+     * Esse método lista todos os atributos de uma instância da classe Sessao no 
+     * console.
+     */
+    public void listarSessao() {
+        System.out.println("ID: " + getId());
+        // System.out.println("Tipo: " + getTipoDeSessao());
+        System.out.println("Nome: " + getNomeSessao());
+        System.out.println("Data/hora: " + getDataHora());
+    }
 }
