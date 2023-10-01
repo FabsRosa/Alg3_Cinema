338d07098da442f23c2332e682f2d9a3e7369858
Gabs <gsbiel62@gmail.com>
2023-10-01 15:42:34 -0400

    — Adicionadas várias bibliotecas para leitura e escrita em arquivos; — Adicionados comentários para melhor documentar o sistema; — Implementada uma nova lógica para vários métodos.

--- Cinema/src/main/java/ufmt_alg3/cinema/Arquivo.java
+++ Cinema/src/main/java/ufmt_alg3/cinema/Arquivo.java
@@ -1,81 +1,108 @@
 package ufmt_alg3.cinema;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
-/**
+/*
  * @author Virgínia Aguiar
- *         Gabriel Soares
- *         Fabrício Rosa
+ * @author Gabriel Soares
+ * @author Fabrício Rosa
  */
 
 public class Arquivo {
     
-    private final String nomeDoArquivo;
-    
-    public Arquivo () {
-        this.nomeDoArquivo = "arquivo\registrosPersistidos.ser";
-    }
+    private final String nomeDoArquivo = "arquivo_registros.dat";
     
     public String getNomeDoArquivo() {
         return nomeDoArquivo;
     }
     
-    public void salvar(ArrayList<Sessao> lista) throws IOException {
-        File arquivo = new File(nomeDoArquivo);
-        
-        // Se o arquivo não existir, então ele é criado.
-        if (!arquivo.exists()) {
-                arquivo.createNewFile();
-        }
-        
-        
-        try (FileOutputStream fileOut = new FileOutputStream(
-                getNomeDoArquivo());
-                ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
-            // Grava o ArrayList no arquivo
-            out.writeObject(lista);
-            
-        }
+    /**
+     * O método {@code salvar} é utilizado para persistir os dados de cadastro
+     * de uma sessão do cinama em um arquivo.
+     * 
+     * @author Gabriel  
+     * @param lista armazena uma lista de cadastros de sessões.
+     * @param nomeArquivo é o nome do arquivo onde os dados serão guardados.
+     * @throws java.io.IOException
+     */
 
-        System.out.println("Dados persistidos com sucesso no arquivo.");
-    }
-
-    public void listar() {
+    public void salvar(ArrayList<Sessao> lista, String nomeArquivo)
+            throws IOException {
+        FileOutputStream fileOut = new FileOutputStream(nomeArquivo);
+        ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+        objOut.writeObject(lista);
         
+        System.out.println("Lista de objetos foi salva no arquivo chamado \""
+                + nomeArquivo + "\" com sucesso!\n");
+    }    
+
+    /**
+     * O método {@code listar} abre um arquivo serializado (.dat), extrai os 
+     * objetos nele armazenados em uma coleção ArrayList. Após extrair tudo de 
+     * dentro dos arquivos, o método usa a lista de cópia para ler os dados e
+     * imprimí-los no console.
+     * 
+     * @param nomeArquivo
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws ClassNotFoundException
+    */
+    
+    public void listar(String nomeArquivo) throws FileNotFoundException,
+            IOException, ClassNotFoundException {
+        ArrayList<Sessao> listaSessoes; 
+        FileInputStream fileIn = new FileInputStream(nomeArquivo);
+        ObjectInputStream objIn = new ObjectInputStream(fileIn);
+        listaSessoes = (ArrayList<Sessao>) objIn.readObject();
+        
+        System.out.println("Lista de sessões cadastradas no arquivo: \n");
+        for (Sessao sessaoAtual : listaSessoes) {
+            sessaoAtual.listarSessao();
+        }   
     }
 
     public void limpar() {
         
     }
     
-    public Integer buscarUltimoId() throws IOException, ClassNotFoundException {
-        // Variáveis locais auxiliares.
-        String nomeArquivo;
-        Integer ultimoId;
-        nomeArquivo = getNomeDoArquivo();
+    
+    /**
+     * O método {@code buscarUltimoId} que abre um arquivo e checa se ele 
+     * existe. Caso exista, ele desserializa os dados dele, guarda-os em uma 
+     * lista e busca pelo último elemento dela. Se não existir, então retorna
+     * 1
+     * 
+     * @param nomeArquivo representa o nome do arquivo a ser buscado o id.
+     * @throws java.io.IOException
+     * @throws java.lang.ClassNotFoundException
+     * @return Caso o arquivo exista, ele retorna um valor Integer que 
+     * representa o último valor cadastrado na lista. Se não existir, então 
+     * retorna 1.
+     */
+    
+    public static Integer buscarUltimoId(String nomeArquivo) throws IOException,
+            ClassNotFoundException {
+        Path caminhoArquivo = Paths.get(nomeArquivo);
         
-        /*
-         * O seguinte trecho de código será utilizado para desserializar os
-         * objetos guardados no arquivo, armazenando-os em uma lista.
-         */
-        FileInputStream arquivo = new FileInputStream(nomeArquivo);
-        try (ObjectInputStream sessao = new ObjectInputStream(arquivo)) {
-            ArrayList<Sessao> listaDoArq = (ArrayList<Sessao>)
-                    sessao.readObject();
-            
-            // Verifica se o arquivo não está vazio.
-            if (!listaDoArq.isEmpty()) {
-                // Obtém o último objeto da lista
-                Sessao ultimoObjeto = listaDoArq.get(listaDoArq.size() - 1);
-
-                // Acessa o atributo desejado do último registro
-                ultimoId = ultimoObjeto.getId();
-                return ultimoId;
-            } else {
-                System.out.println("O arquivo está vazio.");
-            }
+        if (Files.exists(caminhoArquivo)) {
+            ArrayList<Sessao> listaSessoes; 
+            FileInputStream fileIn = new FileInputStream(nomeArquivo);
+            ObjectInputStream objIn = new ObjectInputStream(fileIn);
+            listaSessoes = (ArrayList<Sessao>) objIn.readObject();
+        
+            return (listaSessoes.get(listaSessoes.size()).getId());
+        } else {
+            System.out.println("O arquivo não existe. Retornando 1.");
+            return 1;
         }
-        return null;
     }
 }
